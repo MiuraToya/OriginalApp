9621075d453de544fe5429b9481c8dc9a06c3681
Miura Toya <dengzais8@gmail.com>
2020-10-22 13:36:48 +0900

    Initial Commit

---
+++ OriginalTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OriginalTask
+//
+//  Created by 三浦　登哉 on 2020/10/22.
+//  Copyright © 2020 toya.miura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
